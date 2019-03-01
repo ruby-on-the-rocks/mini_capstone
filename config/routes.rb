c3e97854719f15cf8472bf52507fb3971bed5992
Peter Jang <peter.x.jang@gmail.com>
2019-02-28 18:38:40 -0600

    Use body params to display an individual product by id

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     get "/all_products_url" => "products#all_products_method"
     get "/one_product_query_url" => "products#one_product_method"
     get "/one_product_segment_url/:id" => "products#one_product_method"
+    post "/one_product_body_url" => "products#one_product_method"
   end
 end
