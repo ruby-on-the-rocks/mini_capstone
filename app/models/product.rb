48ea3689cdb33cf59907e9687ad3fb9690efb7a7
Peter Jang <peter.x.jang@gmail.com>
2019-03-04 21:29:49 -0600

    Add validations and create happy/sad path

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 2..500 }
+
   def is_discounted?
     price <= 10
   end
