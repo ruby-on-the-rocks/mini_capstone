fd372fc2e9ec0d7a3ebafad0d8c1a51206eef3cc
Peter Jang <peter.x.jang@gmail.com>
2019-03-07 18:36:05 -0600

    Add filter and sort options for products index action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,23 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    if params["search"]
+      @products = @products.where("name ILIKE ?", "%#{params["search"]}%")
+    end
+
+    if params["discount"]
+      @products = @products.where("price < ?", 10)
+    end
+
+    if params["sort"] == "price" && params["sort_order"] == "asc"
+      @products = @products.order(:price => :asc)
+    elsif params["sort"] == "price" && params["sort_order"] == "desc"
+      @products = @products.order(:price => :desc)
+    else
+      @products = @products.order(:id => :desc)
+    end
+
     render "index.json.jbuilder"
   end
 
