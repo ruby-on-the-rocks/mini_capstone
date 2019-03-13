2eb51c8c2d1bda6cf3283e6c740b2e7abf557cd6
Peter Jang <peter.x.jang@gmail.com>
2019-03-12 20:23:09 -0500

    Add orders create action

---
+++ app/controllers/api/orders_controller.rb
@@ -0,0 +1,20 @@
+class Api::OrdersController < ApplicationController
+  def create
+    product = Product.find_by(id: params[:product_id])
+
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
+    )
+    @order.save
+    render "show.json.jbuilder"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     get "/products/:id" => "products#show"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
+
+    post "/orders" => "orders#create"
   end
 end
