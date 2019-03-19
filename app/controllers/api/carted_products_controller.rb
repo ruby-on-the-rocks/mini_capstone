ef7b831dc9aa7145e1f46a4e58157c52ba4c2862
Peter Jang <peter.x.jang@gmail.com>
2019-03-18 19:32:54 -0500

    Add carted products index action

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,4 +1,18 @@
 class Api::CartedProductsController < ApplicationController
+  def index
+    # @carted_products = []
+    # all_carted_products= CartedProduct.all
+    # all_carted_products.each do |carted_product|
+    #   if carted_product.user_id == current_user.id && carted_product.status == "carted"
+    #     @carted_products << carted_product
+    #   end
+    # end
+    # @carted_products = CartedProduct.where(user_id: current_user.id).where(status: "carted")
+    # @carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
+    @carted_products = current_user.carted_products.where(status: "carted")
+    render "index.json.jbuilder"
+  end
+
   def create
     @carted_product = CartedProduct.new(
       product_id: params[:product_id],
---
+++ app/views/api/carted_products/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.array! @carted_products.each do |carted_product|
+  json.id carted_product.id
+  json.product_id carted_product.product_id
+  json.quantity carted_product.quantity
+  json.status carted_product.status
+  json.order_id carted_product.order_id
+end
