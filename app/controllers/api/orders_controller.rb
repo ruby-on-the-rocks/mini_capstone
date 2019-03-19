d1c69d82a3da9721afc967dbcf6addb5282dc8e8
Peter Jang <peter.x.jang@gmail.com>
2019-03-18 20:30:38 -0500

    Refactor orders create method to move logic to model

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,29 +7,12 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    carted_products = current_user.carted_products.where(status: "carted")
-
-    calculated_subtotal = 0
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-    end
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.new(
-      user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
+      user_id: current_user.id
     )
     @order.save
-
-    carted_products.each do |carted_product|
-      carted_product.status = "purchased"
-      carted_product.order_id = @order.id
-      carted_product.save
-    end
-    
+    carted_products = current_user.carted_products.where(status: "carted")
+    @order.update_totals(carted_products)
     render "show.json.jbuilder"
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,24 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :carted_products
   has_many :products, through: :carted_products
+
+  def update_totals(carted_products)
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+  
+    self.subtotal = calculated_subtotal
+    self.tax = calculated_tax
+    self.total = calculated_total
+    self.save
+
+    carted_products.each do |carted_product|
+      carted_product.status = "purchased"
+      carted_product.order_id = self.id
+      carted_product.save
+    end
+  end
 end
