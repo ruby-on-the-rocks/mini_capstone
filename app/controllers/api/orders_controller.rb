1ba3d97752a46c960d357428653962eb516ced08
Peter Jang <peter.x.jang@gmail.com>
2019-03-18 19:52:54 -0500

    Add order create action (using shopping cart)

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,21 +7,29 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
+    carted_products = current_user.carted_products.where(status: "carted")
 
-    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
     calculated_tax = calculated_subtotal * 0.09
     calculated_total = calculated_subtotal + calculated_tax
 
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       subtotal: calculated_subtotal,
       tax: calculated_tax,
-      total: calculated_total
+      total: calculated_total,
     )
     @order.save
+
+    carted_products.each do |carted_product|
+      carted_product.status = "purchased"
+      carted_product.order_id = @order.id
+      carted_product.save
+    end
+    
     render "show.json.jbuilder"
   end
 
--- app/views/api/orders/show.json.jbuilder
+++ app/views/api/orders/show.json.jbuilder
@@ -1,7 +1,5 @@
 json.id @order.id
 json.user_id @order.user_id
-json.product @order.product
-json.quantity @order.quantity
 json.subtotal @order.subtotal
 json.tax @order.tax
 json.total @order.total
